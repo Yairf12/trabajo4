6165782762264c95e30d4b60722a62b6d9d1501a
Yairf12 <133272941+Yairf12@users.noreply.github.com>
2023-06-15 14:34:46 -0600

    Actualizar empleadoporcomision.java

--- ejercicio 4/src/ejercicio/pkg3/empleadoporcomision.java
+++ ejercicio 4/src/ejercicio/pkg3/empleadoporcomision.java
@@ -1,75 +1,80 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package ejercicio.pkg3;
+package proyectoherencia3;
 
-/**
- *
- * @author Uciel
- */
-public class Facturas {
-        public String numeroPieza;
-    public String descripcionPieza;
-    public int cantidadArticulos;
-    public double precioArticulo;
-    
-    public Facturas(String numeroPieza, String descripcionPieza, int cantidadArticulos, double precioArticulo) {
-        this.numeroPieza = numeroPieza;
-        this.descripcionPieza = descripcionPieza;
-        this.cantidadArticulos = cantidadArticulos;
-        this.precioArticulo = precioArticulo;
-    }
-    
-    public String getNumeroPieza() {
-        return numeroPieza;
-    }
-    
-    public void setNumeroPieza(String numeroPieza) {
-        this.numeroPieza = numeroPieza;
-    }
-    
-    public String getDescripcionPieza() {
-        return descripcionPieza;
-    }
-    
-    public void setDescripcionPieza(String descripcionPieza) {
-        this.descripcionPieza = descripcionPieza;
-    }
+public class EmpleadoBaseMasComision {
+    private final String primerNombre;
+    private final String apellidoPaterno;
+    private final String numeroSeguroSocial;
+    private double ventasBrutas;
+    private double tarifaComision;
+    private double salarioBase;
 
-    public int getCantidadArticulos() {
-        return cantidadArticulos;
-    }
-
-    public void setCantidadArticulos(int cantidadArticulos) {
-        this.cantidadArticulos = cantidadArticulos;
-    }
-
-    public double getPrecioArticulo(double par) {
-        return precioArticulo;
-    }
-
-    public void setPrecioArticulo(double precioArticulo) {
-        if (precioArticulo<0){
-            System.out.println("0.0");
-        } else{
-            this.precioArticulo = precioArticulo;
-        }
-    }
-    
-    public Facturas() {
-    }
-    
-    public double obtenerMontoFactura(){
-        double nc;
+    public EmpleadoBaseMasComision(String primerNombre, String apellidoPaterno, String numeroSeguroSocial, double ventasBrutas, double tarifaComision, double salarioBase) {
+        if(ventasBrutas<0.0)
+        throw new IllegalArgumentException("Las ventas brutas deben de ser >= a 0.0 ");
+        if(tarifaComision <= 0.0 || tarifaComision >= 1.0)
+        throw new IllegalArgumentException("La tarifa de comision debe ser > 0.0 y < 1.0 ");
+        if (salarioBase < 0.0)
+        throw new IllegalArgumentException("El salario base debe ser >= 0.0 ");
         
-        nc= (cantidadArticulos * precioArticulo);
-        System.out.println("El total a pagar por su compra es: ");
-        
-        if(nc<0){
-            System.out.println("0");
-        }
-        return nc;
+        this.primerNombre = primerNombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.numeroSeguroSocial = numeroSeguroSocial;
+        this.ventasBrutas = ventasBrutas;
+        this.tarifaComision = tarifaComision;
+        this.salarioBase = salarioBase;
     }
-
+    
+    public String obtenerPrimerNombre(){
+        return primerNombre;
+    }
+    
+    public String obtenerApellidoPaterno(){
+        return apellidoPaterno;
+    }
+    
+    public String obtenerNumeroSeguroSocial(){
+        return numeroSeguroSocial;
+    }
+    
+    public void establecerVentasBrutas(double ventasBrutas){
+        if(ventasBrutas < 0.0)
+            throw new IllegalArgumentException("Las ventas brutas deben de ser >= a 0.0 ");
+        this.ventasBrutas=ventasBrutas;
+    }
+    
+    public double obtenerVentasBrutas(){
+        return ventasBrutas;
+    }
+    
+    public void establecerTarifaComision(double tarifaComision){
+        if(tarifaComision <= 0.0 || tarifaComision >= 1.0)
+            throw new IllegalArgumentException("La tarifa de comision debe ser > 0.0 y < 1.0 ");
+        this.tarifaComision=tarifaComision;
+    }
+    
+    public double obtenerTarifaComison(){
+        return tarifaComision;
+    }
+    
+    public void establecerSalarioBase(double salarioBase){
+        if (salarioBase < 0.0)
+            throw new IllegalArgumentException("El salario base debe ser >= 0.0 ");
+        this.salarioBase=salarioBase;
+    }
+    
+    public double obtenerSalarioBase(){
+        return salarioBase;
+    }
+    
+    public double ingresos(){
+        return salarioBase + (tarifaComision * ventasBrutas);
+    }
+    
+    @Override
+    public String toString(){
+        return String.format("%s: %s %s%n%s: %s%n%s: %.2f%n%s: %.2f%n%s: 2f",
+                "empleado por comision con sueldo base", primerNombre, apellidoPaterno,
+                "numero de seguro social", numeroSeguroSocial, "ventas brutas", ventasBrutas,
+                "tarifa de comision", tarifaComision, "salario base", salarioBase);
+    }
 }
